af4ffdb7ea4be74899529b133c1cecd5f4d76dd7
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2023-11-03 15:04:25 +0100

    Buscar por codigo de  pais

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { CountriesService } from './../../services/countries.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Country } from '../../interfaces/country';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styleUrls: ['./country-page.component.scss']
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
 
+  public country?: Country;
+
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private router: Router,
+    private countriesService: CountriesService,
+  ) { }
+
+
+  ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(
+        switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id))
+      )
+      .subscribe(country => {
+        if (!country) {
+          return this.router.navigateByUrl('');
+        } else {
+          console.log(country);
+          return this.country = country;
+          return;
+        }
+      })
+  }
 }
