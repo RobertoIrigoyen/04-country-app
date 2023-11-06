68d8aaab8400a0b19ca0c9ad85c5d3bf1b5aa0fd
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2023-11-06 13:59:53 +0100

    busqueda por regiones click

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,22 +2,30 @@ import { CountriesService } from './../../services/countries.service';
 import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania'
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
   styleUrls: ['./by-region-page.component.scss']
 })
+
 export class ByRegionPageComponent {
 
-  public placeHolder: string = "Buscar por región"
+  public placeHolder: string = "Buscar por región";
 
-  public countries: Country[] = []
+  public countries: Country[] = [];
+
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) {
 
   }
 
-  searchByRegion(term: string) {
+  searchByRegion(term: Region) {
+
+    this.selectedRegion = term;
     this.countriesService.searchRegion(term)
       .subscribe(countries => {
         this.countries = countries;
