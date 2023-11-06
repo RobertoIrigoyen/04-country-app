76d9d72d3d90af9a27aa1773785982d442354267
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2023-11-06 13:39:24 +0100

    Busqueda activa conforme escribimos y limpieza de suscripcion

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,11 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, OnDestroy } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.scss']
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
+
+
+  private debouncer: Subject<string> = new Subject<string>()
+
+  private debouncerSuscribe?: Subscription;
 
   @Input()
   public placeholder: string = "";
@@ -13,7 +19,28 @@ export class SearchBoxComponent {
   @Output()
   public onValue = new EventEmitter<string>()
 
+  @Output()
+  public onDebounce = new EventEmitter<string>()
+
+  ngOnInit(): void {
+    this.debouncerSuscribe = this.debouncer
+      .pipe(
+        debounceTime(500)
+      )
+      .subscribe(value => {
+        this.onDebounce.emit(value)
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSuscribe?.unsubscribe()
+  };
+
   emitValue(value: string): void {
     this.onValue.emit(value)
+  }
+
+  onKeyPress(searchTerm: string) {
+    this.debouncer.next(searchTerm)
   }
 }
